4b0dd7b4b4934829b48dbea3f7c1e583f6fae2e7
Charrier Simon <150603972+TrappyMadz@users.noreply.github.com>
2025-05-12 21:26:34 +0200

    MiniGame4Done

---
+++ protoJeu/Assets/Scripts/TimerGame.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TimerGame : MonoBehaviour
+{
+    [SerializeField] private TMP_Text instructionText;
+    [SerializeField] private TMP_Text timerText;
+    [SerializeField] private TMP_Text objectiveText;
+    [SerializeField] private float maxStartValue;
+    [SerializeField] private float minStartValue;
+
+    private float timer;
+    private float objective;
+    private bool timerActive;
+
+    private MainManager mainManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainManager = FindObjectOfType<MainManager>();
+        StartCoroutine(MiniGameStart());
+        
+    }
+
+    private void Update()
+    {
+        Timer();
+    }
+
+    private IEnumerator MiniGameStart()
+    {
+        yield return new WaitForSeconds(0.5f);
+        instructionText.gameObject.SetActive(false);
+        objective = Random.Range(minStartValue, maxStartValue);
+        objectiveText.text = "00:0" + Mathf.Round(objective).ToString();
+        timer = 5;
+        timerActive = true;
+    }
+
+    public void Timer()
+    {
+        if (timerActive && timer > 0)
+        {
+            timer -= Time.deltaTime;
+            timerText.text = "00:0" + Mathf.Round(timer).ToString();
+        }
+        else if (timerActive && timer <= 0)
+        {
+            mainManager.MiniGameLost();
+        }
+    }
+
+    public void StopButtonPressed()
+    {
+        timerActive = false;
+
+        if (Mathf.Round(timer) == Mathf.Round(objective))
+        {
+            mainManager.MiniGameWon();
+        }
+        else
+        {
+            mainManager.MiniGameLost();
+        }
+    }
+}
