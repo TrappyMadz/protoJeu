78e07904ba4e68d4e622ef72edcdcbb59e6c2dd7
Charrier Simon <150603972+TrappyMadz@users.noreply.github.com>
2025-05-13 09:15:57 +0200

    FinishedCowBowMiniGame

---
+++ protoJeu/Assets/Scripts/CowBowGame.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoBowGame : MonoBehaviour
+{
+    [SerializeField] private TMP_Text instructionText;
+    [SerializeField] private int targetToGet;
+    [SerializeField] private float targetsPositionRangeX;
+    [SerializeField] private float targetsPositionRangeY;
+
+    private Button[] targets;
+    private int targetClicked;
+
+    private MainManager mainManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainManager = FindObjectOfType<MainManager>();
+
+        targetClicked = 0;
+        targets = FindObjectsOfType<Button>();
+        for (int i = 0; i < targets.Length; i++)
+        {
+            RectTransform rectTransform = targets[i].GetComponent<RectTransform>();
+            rectTransform.anchoredPosition = new Vector2(Random.Range(-targetsPositionRangeX, targetsPositionRangeX), Random.Range(-targetsPositionRangeY, targetsPositionRangeY));
+        }
+        StartCoroutine(MiniGameStart());
+    }
+
+
+    private IEnumerator MiniGameStart()
+    {
+        yield return new WaitForSeconds(0.5f);
+        instructionText.gameObject.SetActive(false);
+    }
+
+    public void TargetClicked(GameObject target)
+    {
+        Destroy(target.gameObject);
+        targetClicked++;
+        if (targetClicked >= targetToGet)
+        {
+            mainManager.instance.MiniGameWon();
+        }
+    }
+}
