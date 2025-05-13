7bce91969bed4a73310d72a7e488e62afa28caef
Charrier Simon <150603972+TrappyMadz@users.noreply.github.com>
2025-05-13 11:58:08 +0200

    Added score on victory and added a target to target minigame

--- protoJeu/Assets/Scripts/VictoryButtons.cs
+++ protoJeu/Assets/Scripts/VictoryButtons.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class VictoryButtons : MonoBehaviour
 {
     [SerializeField] private string MainMenu = "MenuGame";
+    [SerializeField] private TMP_Text scoreText;
+
+    private MainManager mainManager;
+
+    private void Start()
+    {
+        mainManager = FindObjectOfType<MainManager>();
+        mainManager.instance.SetPlaying(false);
+
+        float timer = mainManager.instance.GetTrueTimer();
+        string seconds = Mathf.Round(timer).ToString();
+        string milliseconds = (Mathf.Round((timer - Mathf.Floor(timer)) * 1000).ToString());
+        
+
+        scoreText.text = $"Temps restant : {seconds}:{milliseconds}";
+    }
     public void Replay()
     {
         SceneManager.LoadScene(MainMenu);
     }
 
-    public void OnApplicationQuit()
+    public void ButtonQuit()
     {
-        OnApplicationQuit();
+        Application.Quit();
     }
 }
