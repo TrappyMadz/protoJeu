e136be0d80d79dbf31f88e477641dabc83770db0
Charrier Simon <150603972+TrappyMadz@users.noreply.github.com>
2025-05-14 17:01:33 +0200

    New Mini Game Bitches

--- protoJeu/Assets/Scripts/HeadTails.cs
+++ protoJeu/Assets/Scripts/HeadTails.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,9 +9,13 @@ public class HeadTails : MonoBehaviour
 {
     [SerializeField] private TMP_Text globalTimer;
     [SerializeField] private TMP_Text instructionText;
-    [SerializeField] private float timeToFinish;
+    [SerializeField] private Button left;
+    [SerializeField] private Button right;
+    [SerializeField] private GameObject watchout;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject wall;
 
-    private float timePassed;
+    private int playerChoice = 0;
 
     private MainManager mainManager;
 
@@ -18,9 +23,10 @@ public class HeadTails : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        wall.GetComponent<Rigidbody2D>().gravityScale = 0;
+        watchout.SetActive(false);
         mainManager = FindObjectOfType<MainManager>();
         mainManager.instance.SetGlobalTimer(globalTimer);
-        timePassed = 0;
         StartCoroutine(MiniGameStart());
     }
 
@@ -29,14 +35,62 @@ public class HeadTails : MonoBehaviour
     {
         yield return new WaitForSeconds(0.5f);
         instructionText.gameObject.SetActive(false);
-    }
+        int rnd = Random.Range(1, 3);
 
-    private void Update()
-    {
-        timePassed += Time.deltaTime;
-        if (timePassed > timeToFinish)
+        if (rnd == 1)
+        {
+            TeleportAnim(left.GetComponent<RectTransform>().anchoredPosition, watchout);
+        }
+        else
+        {
+            TeleportAnim(right.GetComponent<RectTransform>().anchoredPosition, watchout);
+        }
+        yield return new WaitForSeconds(0.5f);
+
+        GameEnd(rnd);
+
+        yield return new WaitForSeconds(.7f);
+
+        if (player.transform.position.y < -350)
         {
             mainManager.instance.MiniGameLost();
+        }
+        else
+        {
+            mainManager.instance.MiniGameWon();
+        }
+    }
+
+    public void TeleportAnim(Vector2 position, GameObject watchout)
+    {
+        watchout.GetComponent<RectTransform>().anchoredPosition = position;
+        watchout.SetActive(true);
+    }
+
+    public void playerClicked(int choice)
+    {
+        playerChoice = choice;
+        if (choice == 1)
+        {
+            TeleportAnim(left.GetComponent<RectTransform>().anchoredPosition, player);
+        }
+        else
+        {
+            TeleportAnim(right.GetComponent<RectTransform>().anchoredPosition, player);
+        }
+    }
+
+    public void GameEnd(int notSafePlace)
+    {
+        if (notSafePlace == 1)
+        {
+            TeleportAnim(new Vector2(-460, 590), wall);
+            wall.GetComponent<Rigidbody2D>().gravityScale = 1000;
+        }
+        else
+        {
+            TeleportAnim(new Vector2(460, 590), wall);
+            wall.GetComponent<Rigidbody2D>().gravityScale = 1000;
         }
     }
 }
