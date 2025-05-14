7db45dbd8ed0afd141f38a665c632858ca601ae3
Charrier Simon <150603972+TrappyMadz@users.noreply.github.com>
2025-05-14 15:04:14 +0200

    RandomizedSilhouette

--- protoJeu/Assets/Scripts/MiniGame1.cs
+++ protoJeu/Assets/Scripts/MiniGame1.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MiniGame1 : MonoBehaviour
 {
@@ -9,6 +10,14 @@ public class MiniGame1 : MonoBehaviour
     [SerializeField] private float timeToFinish;
     [SerializeField] private TMP_Text timeToFinishText;
     [SerializeField] private TMP_Text instructionText;
+    [SerializeField] private Vector2 pos1;
+    [SerializeField] private Vector2 pos2;
+    [SerializeField] private Vector2 pos3;
+    [SerializeField] private Vector2 pos4;
+
+
+    private List<Button> buttonList = new List<Button>();
+    private List<RectTransform> buttonRctTrsfm = new List<RectTransform>();
     private float timePassed;
     private MainManager mainManager;
    void Start()
@@ -43,7 +52,34 @@ public class MiniGame1 : MonoBehaviour
 
     private IEnumerator StartGame()
     {
-        yield return new WaitForSeconds(0.5f);
         instructionText.gameObject.SetActive(false);
+        ButtonSwitch();
+        yield return new WaitForSeconds(0.5f);
+       
     }
-}
+
+    public void ButtonSwitch()
+    {
+        Button[] buttonArray = FindObjectsOfType<Button>();
+        Shuffle(buttonArray);
+        buttonArray[0].GetComponent<RectTransform>().anchoredPosition = pos1;
+        buttonArray[1].GetComponent<RectTransform>().anchoredPosition = pos2;
+        buttonArray[2].GetComponent<RectTransform>().anchoredPosition = pos3;
+        buttonArray[3].GetComponent<RectTransform>().anchoredPosition = pos4;
+
+    }
+
+
+    public void Shuffle(Button[] array)
+        {
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                int randIndex = Random.Range(0, i + 1);
+                (array[i], array[randIndex]) = (array[randIndex], array[i]);
+            }
+        }
+
+
+    }
+
+    
